252d142fc97d515ffbd9b9644443602391705e02
Yauhen Vasilyeu <yauhen.vasilyeu@gmail.com>
2019-03-24 20:15:39 +0300

    feat: add some code for testing in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let int = 2;
+  let index = 0;
+  let arr = [];
+  let count = 0;
+  while (base > 1)
+  {
+      while (base % int == 0)
+      {
+        base = base / int;
+        arr.push(int);
+      }
+      int++;
+  }
+
+  for(let i = 0; i < arr.length; ++i){
+      if(arr[i] == arr[arr.length - 1])
+        count++;
+  }
+
+  while(number > 0)
+  {
+      if(number>1){
+        number /= int-1;
+        index += Math.floor(number);
+      }else{
+        break;
+      }
+  }
+  return Math.floor(index/count);
 }
